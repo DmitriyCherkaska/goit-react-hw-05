981d20e516c6d949b60cd0e4956d49c0e7bc6090
DmitriyCherkaska <151064884+DmitriyCherkaska@users.noreply.github.com>
2024-06-16 21:08:49 +0300

    dfg

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,22 +1,22 @@
-import ImageCard from "../ImageCard/ImageCard";
-import some from "./ImageGallery.module.css";
+const HomePage = () => {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNGI5YzQwYTU1YTc5MTQ2M2VlNGQ1ZWZjYjk1OWE5MiIsInN1YiI6IjY2NmYxODIzYmUwYThkY2Y5YzkzMGMwMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hhnRXCL3m7Wky9HcRgX6zFoMzK2BaBQHiqwxUYnFI-4',
+    },
+  };
 
-const ImageGallery = ({ images, onImageClick }) => {
-  return (
-    <ul className={some.list}>
-      {images.map((image) => (
-        <li key={image.id}>
-          <ImageCard
-            id={image.id}
-            selectedImage={image.urls.regular}
-            alt={image.alt_description}
-            srcSmall={image.urls.small}
-            onImageClick={onImageClick}
-          />
-        </li>
-      ))}
-    </ul>
-  );
+  fetch(
+    'https://api.themoviedb.org/3/trending/movie/day?language=en-US',
+    options,
+  )
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+  return <div>HomePage</div>;
 };
 
-export default ImageGallery;
+export default HomePage;
